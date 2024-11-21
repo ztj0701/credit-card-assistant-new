d5dd039cde054877a0055bbe60dca3fbe660fb9d
ztj0701 <131656510+ztj0701@users.noreply.github.com>
2024-11-21 17:03:08 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // In development and production, API calls will be made to the same origin
-const API_URL = 'https://chatbot-by-bolt-cf6611d22e97.herokuapp.com';  // Empty string means same origin
+const API_URL = 'https://credit-card-assistant-new-4c1b62209060.herokuapp.com/';  // Empty string means same origin
 export { API_URL };
